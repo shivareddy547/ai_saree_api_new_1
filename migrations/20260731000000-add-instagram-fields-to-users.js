f765a1afb2619fb605df9ae274d4adfc1d5e9a60
Shivareddy Adudodla <sadudodla@aligntech.com>
2026-08-04 17:21:03 +0530

    Changes from YAML response

--- migrations/20260731000000-add-instagram-fields-to-users.js
+++ migrations/20260731000000-add-instagram-fields-to-users.js
@@ -1,38 +1,88 @@
 'use strict';
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // ADD COLUMN: instagramAccessToken to users table
-    await queryInterface.addColumn('users', 'instagramAccessToken', {
-      type: Sequelize.STRING,
-      allowNull: true
-    });
-    // ADD COLUMN: instagramAccountId to users table
-    await queryInterface.addColumn('users', 'instagramAccountId', {
-      type: Sequelize.STRING,
-      allowNull: true
-    });
-    // ADD COLUMN: instagramUsername to users table
-    await queryInterface.addColumn('users', 'instagramUsername', {
-      type: Sequelize.STRING,
-      allowNull: true
-    });
-    // ADD COLUMN: instagramAccountType to users table
-    await queryInterface.addColumn('users', 'instagramAccountType', {
-      type: Sequelize.STRING,
-      allowNull: true
-    });
-    // ADD COLUMN: instagramTokenExpiresAt to users table
-    await queryInterface.addColumn('users', 'instagramTokenExpiresAt', {
-      type: Sequelize.DATE,
-      allowNull: true
-    });
+    const tableName = 'users';
+    const tableDefinition = await queryInterface.describeTable(tableName);
+
+    // Add instagramAccessToken if it doesn't exist
+    if (!tableDefinition.instagramAccessToken) {
+      await queryInterface.addColumn(tableName, 'instagramAccessToken', {
+        type: Sequelize.STRING,
+        allowNull: true
+      });
+    }
+
+    // Add instagramAccountId if it doesn't exist
+    if (!tableDefinition.instagramAccountId) {
+      await queryInterface.addColumn(tableName, 'instagramAccountId', {
+        type: Sequelize.STRING,
+        allowNull: true
+      });
+    }
+
+    // Add instagramUsername if it doesn't exist
+    if (!tableDefinition.instagramUsername) {
+      await queryInterface.addColumn(tableName, 'instagramUsername', {
+        type: Sequelize.STRING,
+        allowNull: true
+      });
+    }
+
+    // Add instagramAccountType if it doesn't exist
+    if (!tableDefinition.instagramAccountType) {
+      await queryInterface.addColumn(tableName, 'instagramAccountType', {
+        type: Sequelize.STRING,
+        allowNull: true
+      });
+    }
+
+    // Add instagramTokenExpiresAt if it doesn't exist
+    if (!tableDefinition.instagramTokenExpiresAt) {
+      await queryInterface.addColumn(tableName, 'instagramTokenExpiresAt', {
+        type: Sequelize.DATE,
+        allowNull: true
+      });
+    }
   },
-  down: async (queryInterface, Sequelize) => {
-    // Reverse: Remove all Instagram fields from users table
-    await queryInterface.removeColumn('users', 'instagramAccessToken');
-    await queryInterface.removeColumn('users', 'instagramAccountId');
-    await queryInterface.removeColumn('users', 'instagramUsername');
-    await queryInterface.removeColumn('users', 'instagramAccountType');
-    await queryInterface.removeColumn('users', 'instagramTokenExpiresAt');
+
+  down: async (queryInterface) => {
+    const tableName = 'users';
+    const tableDefinition = await queryInterface.describeTable(tableName);
+
+    if (tableDefinition.instagramAccessToken) {
+      await queryInterface.removeColumn(
+        tableName,
+        'instagramAccessToken'
+      );
+    }
+
+    if (tableDefinition.instagramAccountId) {
+      await queryInterface.removeColumn(
+        tableName,
+        'instagramAccountId'
+      );
+    }
+
+    if (tableDefinition.instagramUsername) {
+      await queryInterface.removeColumn(
+        tableName,
+        'instagramUsername'
+      );
+    }
+
+    if (tableDefinition.instagramAccountType) {
+      await queryInterface.removeColumn(
+        tableName,
+        'instagramAccountType'
+      );
+    }
+
+    if (tableDefinition.instagramTokenExpiresAt) {
+      await queryInterface.removeColumn(
+        tableName,
+        'instagramTokenExpiresAt'
+      );
+    }
   }
 };
